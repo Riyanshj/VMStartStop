eb4f6b7b57ddb600c09c15751c7439a42f3bfe63
Riyansh Jain <103922191+Riyanshj@users.noreply.github.com>
2024-06-24 19:55:16 +0530

    Create start-vm.ps1

---
+++ start-vm.ps1
@@ -0,0 +1,11 @@
+# Variables
+$resourceGroup = "myResourceGroup"
+$vmName = "vm01"
+
+# Login to Azure using service principal
+az login --service-principal -u $env:AZURE_CLIENT_ID -p $env:AZURE_CLIENT_SECRET --tenant $env:AZURE_TENANT_ID
+
+# Start the VM
+az vm start --resource-group $resourceGroup --name $vmName
+
+
