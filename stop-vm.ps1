baaf89b423baf79529e614b2f3c9eae8643dcaf4
Riyansh Jain <103922191+Riyanshj@users.noreply.github.com>
2024-06-24 19:56:19 +0530

    Update stop-vm.ps1

--- stop-vm.ps1
+++ stop-vm.ps1
@@ -1,1 +1,9 @@
-az vm deallocate --resource-group "yourResourceGroupName" --name "yourVMName"
+# Variables
+$resourceGroup = "myResourceGroup"
+$vmName = "vm01"
+
+# Login to Azure using service principal
+az login --service-principal -u $env:AZURE_CLIENT_ID -p $env:AZURE_CLIENT_SECRET --tenant $env:AZURE_TENANT_ID
+
+# Start the VM
+az vm deallocate --resource-group $resourceGroup --name $vmName
